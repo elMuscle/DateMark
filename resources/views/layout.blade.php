26ee42e7e5c5263d5b7292817ebd560be1f2cd7b
elMuscle <125409395+elMuscle@users.noreply.github.com>
2023-02-15 11:21:11 +0100

    Created Layout.blade.php

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    @include('partials._fileheader')
+    <title>@yield('title')</title>
+  </head>
+  <body class="m4-cloak">
+    <div class="container-fluid">
+
+            @include('partials._adminmenu')
+
+        <!-- Header -->
+        @include('partials._maintenance')
+        <header>
+            <div class="row flex-align-center">
+            <div class="cell-3 cell-md-one-third p-2"><a href="{{ route('home.index') }}"><img class="mw-75-md mw-50-xl d-block mx-auto mr-0-md" src="{{ url('img/logo-owr.svg') }}" ></a></div>
+            <div class="cell-9 cell-md-two-third p-2 pt-5"><h1><span class="fg-owrRed">ÖWR Planungstool LV Wien</span> <br /> <small class="d-none d-inline-md">Einsätze, Veranstaltungen, ...</small></h1></div>
+            </div>
+            <div>
+            @yield('menu')
+            </div>
+        </header>
+        <!-- Inhalt -->
+        <div>
+            @yield('inhalt')
+            @include('partials._footer')
+        </div>
+    </div>
+    <!-- Metro 4 -->
+    <script src="{{ url('js/metro.min.js') }}"></script>
+    @yield('scripts')
+  </body>
+</html>
+
+
