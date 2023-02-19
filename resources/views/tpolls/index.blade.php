608b623505b6734fcd59e4cebe1c2622e4448629
elMuscle <125409395+elMuscle@users.noreply.github.com>
2023-02-19 21:24:46 +0100

    Update index.blade.php
    Changed box spacing and optimized for device-sizes

--- resources/views/tpolls/index.blade.php
+++ resources/views/tpolls/index.blade.php
@@ -6,23 +6,25 @@
     </x-slot>
 
     <!-- Print out Tpolls-->
-    @foreach ($tpolls as $tpoll)
     <div class="container mt-5">
-        <div class="card">
-            <div class="card-body">
-                <div class="row justify-content-between">
-                    <a class="h5 card-title" href="{{ route('tpolls.show',['tpoll'=>$tpoll->id]) }}">{{ $tpoll->titel }}</a>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+            <!-- Loop through Tpolls-->
+            @foreach ($tpolls as $tpoll)
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <div class="row justify-content-between">
+                        <a class="h5 card-title" href="{{ route('tpolls.show',['tpoll'=>$tpoll->id]) }}">{{ $tpoll->titel }}</a>
+                    </div>
+                    <p class="card-text">
+                        <p>{{ $tpoll->info }}</p>
+                        <p>Status: {{ $tpoll->status }}<span class="badge text-bg-success">active</span></p>
+                        <p>Events: {{ $tpoll->events()->count() }}</p>
+                        <button type="button" class="btn btn-outline-warning" onclick="window.location.href = '{{ route('tpolls.edit',['tpoll'=>$tpoll->id]) }}';">Edit</button>
+                        <button type="button" class="btn btn-outline-danger">Delete</button>
+                    </p>
                 </div>
-                <p class="card-text">
-                    <p>{{ $tpoll->info }}</p>
-                    <p>Status: {{ $tpoll->status }}<span class="badge text-bg-success">active</span></p>
-                    <p>Events: {{ $tpoll->events()->count() }}</p>
-                    <button type="button" class="btn btn-outline-warning" onclick="window.location.href = '{{ route('tpolls.edit',['tpoll'=>$tpoll->id]) }}';">Edit</button>
-                    <button type="button" class="btn btn-outline-danger">Delete</button>
-                </p>
             </div>
+            @endforeach
         </div>
     </div>
-
-    @endforeach
 </x-app-layout>
