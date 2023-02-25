c174c98fa5ae4423db84232b7cb7528bdc0f7c9e
elMuscle <125409395+elMuscle@users.noreply.github.com>
2023-02-25 21:43:10 +0100

    Edited tpolls.show

--- resources/views/tpolls/show.blade.php
+++ resources/views/tpolls/show.blade.php
@@ -14,13 +14,13 @@
                 <p>{{ $tpoll->info }}</p>
                 <p>{{ $tpoll->beschreibung }}</p>
             </div>
-            <h4>{{ __('Associated Events') }}:</h4>
+            <h4>{{ __('Events') }}:</h4>
             {{-- Print connected events --}}
             <table class="table table-striped">
                 <thead class="table-primary">
                 <tr>
                     <th scope="col">{{ __('ID') }}</th>
-                    <th scope="col">{{ __('Name') }}</th>
+                    <th scope="col">{{ __('Title') }}</th>
                     <th scope="col">{{ __('Date') }}</th>
                     <th scope="col">{{ __('Time') }}</th>
                     <th scope="col">{{ __('Unlink') }}</th>
@@ -33,7 +33,7 @@
                             <td>{{ $event->id }}</td>
                             <td>{{ $event->was }}</td>
                             <td>{{ $event->ort }}</td>
-                            <td>{{ $event->beginn }} - {{ $event->ende }}</td>
+                            <td>{{ $event->beginn->format('H:i') }} - {{ $event->ende->format('H:i') }}</td>
                             <td><a href="" type="button" class="btn btn-outline-info">{{ __('Unlink') }}</a></td>
                             <td><a href="" type="button" class="btn btn-outline-warning">{{ __('Edit') }}</a></td>
                         </tr>
