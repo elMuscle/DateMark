d6fb93503eab0a343e5fd797a7c0ac83bb14b5be
elMuscle <125409395+elMuscle@users.noreply.github.com>
2023-02-26 11:59:03 +0100

    Added some links, deleted some id columns

--- resources/views/tpolls/show.blade.php
+++ resources/views/tpolls/show.blade.php
@@ -10,7 +10,7 @@
         <div class="p-2 bg-body rounded">
             {{-- Print Tpoll Information --}}
             <div>
-                <h1>{{ $tpoll->titel }}</h1>
+                <a href="{{ route('tpollsguest.show',['tpoll' => $tpoll]) }}"><h1>{{ $tpoll->titel }}</h1></a>
                 <p>{{ $tpoll->info }}</p>
                 <p>{{ $tpoll->beschreibung }}</p>
             </div>
@@ -19,23 +19,21 @@
             <table class="table table-striped">
                 <thead class="table-primary">
                 <tr>
-                    <th scope="col">{{ __('ID') }}</th>
+                    {{-- <th scope="col">{{ __('ID') }}</th> --}}
                     <th scope="col">{{ __('Title') }}</th>
                     <th scope="col">{{ __('Date') }}</th>
                     <th scope="col">{{ __('Time') }}</th>
-                    <th scope="col">{{ __('Unlink') }}</th>
                     <th scope="col">{{ __('Edit') }}</th>
                 </tr>
                 </thead>
                 <tbody class="table-group-divider">
                     @foreach ($events as $event)
                         <tr>
-                            <td>{{ $event->id }}</td>
-                            <td>{{ $event->was }}</td>
+                            {{-- <td>{{ $event->id }}</td> --}}
+                            <td><a href="{{ route('events.show',['event' => $event]) }}">{{ $event->was }}</a></td>
                             <td>{{ $event->ort }}</td>
                             <td>{{ $event->beginn->format('H:i') }} - {{ $event->ende->format('H:i') }}</td>
-                            <td><a href="" type="button" class="btn btn-outline-info">{{ __('Unlink') }}</a></td>
-                            <td><a href="" type="button" class="btn btn-outline-warning">{{ __('Edit') }}</a></td>
+                            <td><a href="{{ route('events.edit',['event' => $event]) }}" type="button" class="btn btn-outline-warning">{{ __('Edit') }}</a></td>
                         </tr>
                     @endforeach
                 </tbody>
