03a738689cc91dafb145abe46e15540925b16d33
elMuscle <125409395+elMuscle@users.noreply.github.com>
2023-03-16 15:48:19 +0100

    Update _footer.blade.php

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="mt-10 row bg-lightGray bd-gray border-top border-size-5 d-none-print">
     <div class="cell-5 cell-md p-2">
-      <span>© Lukas Kubica <span class="d-none d-inline-md"> {{ __('layout.usage') }} &nbsp;<a href="https://metroui.org.ua/" class="fg-black fg-darkGray-hover">Metro 4 UI CSS</a></span></span>
+      <span>Erstellt von Lukas Kubica <span class="d-none d-inline-md"> {{ __('layout.usage') }} &nbsp;<a href="https://metroui.org.ua/" class="fg-black fg-darkGray-hover">Metro 4 UI CSS</a></span></span>
     </div>
     <div class="cell-7 cell-md-4 text-right p-2">
       <a class="fg-black fg-darkGray-hover mx-1" href="{{ route('home.datenschutz') }}">{{ __('layout.privacy') }}</a>
