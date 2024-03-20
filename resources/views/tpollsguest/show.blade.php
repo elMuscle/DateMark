ce2660a23a3605756f8d36b033a2271509cfec3c
OWRKK <164193723+OWRKK@users.noreply.github.com>
2024-03-20 16:31:17 +0100

    tpoll guest nachname zuerst
    In tpoll guest show Vorname-Name getauscht

--- resources/views/tpollsguest/show.blade.php
+++ resources/views/tpollsguest/show.blade.php
@@ -195,7 +195,7 @@ if(isset($nameincookie)){
                     @if (isset($active_member->id) && $member->id == $active_member->id)
                         selected="selected"
                     @endif
-                >{{ $member->name }} {{ str(Str::substr($member->surname, 0, 3))->append('.') }}</option>
+                >{{ $member->surname }} {{ $member->name }}</option>
             @endforeach
         </select>
     </form>
